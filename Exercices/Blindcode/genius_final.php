5a9a421b7588a06573dc4c36c7a77b911096b4cb
ZamBoyle <johnny.piette@gmail.com>
2021-06-09 11:20:57 +0200

    Migration des modifs de genius.php vers genius_final.php

--- Exercices/Blindcode/genius_final.php
+++ Exercices/Blindcode/genius_final.php
@@ -11,19 +11,30 @@ function Substract($number1, $number2)
     return $number1 - $number2;
 }
 
+function Multiply($number1, $number2)
+{
+    return $number1 * $number2;
+}
+
+function Divide($number1, $number2)
+{
+    return $number1 * $number2;
+}
+
 function Display()
 {
     echo "<form action='genius.php' method='post'>";
     if (!empty($_POST["number1"]) && !empty($_POST["number2"])) {
         $number1 = $_POST["number1"];
         $number2 = $_POST["number2"];
-        if(is_numeric($number1) && is_numeric($number2)){
+        if (is_numeric($number1) && is_numeric($number2)) {
             echo "<p>La somme est égale à " . Add($number1, $number2) . ".</p>";
             echo "<p>La soustraction est égale à " . Substract($number1, $number2) . ".</p>";
+            echo "<p>La multiplication est égale à " . Multiply($number1, $number2) . ".</p>";
+            echo "<p>La soustraction est égale à " . Divide($number1, $number2) . ".</p>";
             echo "<input type='submit' value='Recommencer'>";
-        }
-        else{
-            Echo "<p>Les entrées n'étaient pas toutes les deux des nombres.</p><p>Vous avez entré:$number1 et $number2.</p><p>Veuillez recommencer:</p>";
+        } else {
+            echo "<p>Les entrées n'étaient pas toutes les deux des nombres.</p><p>Vous avez entré:$number1 et $number2.</p><p>Veuillez recommencer:</p>";
             DisplayFormControls();
         }
     } else {
@@ -35,8 +46,8 @@ function Display()
 function DisplayFormControls()
 {
     echo "
-    <input name='number1' type='text' id='number1Input'  placeholder='Entrez le premier nombre' required><br/><br/>
-    <input name='number2' type='text' id='number2Input'  placeholder='Entrez le second nombre' required><br/><br/>
+    <input name='number1' type='number' id='number1Input'  placeholder='Entrez le premier nombre' required><br/><br/>
+    <input name='number2' type='number' id='number2Input'  placeholder='Entrez le second nombre' required><br/><br/>
     <input type='submit' value='Envoyer'>
     ";
 }
