1a1ddd56267233818cc78b22402eb0dee33455ed
Johnny Piette <johnny.piette@gmail.com>
2022-03-07 00:09:19 +0100

    Updates

--- Exercices/Blindcode BXL - 2022/genius.js
+++ Exercices/Blindcode BXL - 2022/genius.js
@@ -3,20 +3,7 @@ function add(number1, number2)
     return number1 + number2;
 }
 
-
 function substract(number1, number2)
 {
     return number1 - number2;
-}
-
-function display(str)
-{
-    let display = document.getElementById("display");
-    if(display){
-        display.innerHTML += "<br/><p>"+str+"</p>";
-    }else{
-        display = document.createElement("span");
-        display.id= "display";
-        display.innerHTML = str;
-    }
 }
